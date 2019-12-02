e962fc01d134c7c09fff72941a41da5ee9818572
adgray09 <alexandergray37@yahoo.com>
2019-12-02 13:25:08 -0800

    person can view their submission

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
+from bson.objectid import ObjectId
+
 client = MongoClient()
 db = client.Contractor
 pieces = db.pieces
@@ -8,15 +10,18 @@ pieces = db.pieces
 app = Flask(__name__)
 
 @app.route('/')
+#Home page route
 def store_index():
     art_items = pieces.find()
     return render_template ('art_index.html', art_items=art_items)
 
 @app.route('/art/new')
+#new art submission page
 def new_art():
     return render_template('new_art.html')
 
 @app.route('/pieces', methods=['POST'])
+#submit new art to website
 def submit_art():
     added_art = {
         'title': request.form.get('title'),
@@ -25,7 +30,12 @@ def submit_art():
     }
     print(added_art)
     pieces.insert_one(added_art)
-    return redirect(url_for('store_index'))
+    return redirect(url_for('store_index')) # <--------- has to be the app.route's method that renders art_index
+
+@app.route('/art/<piece_id>')
+def art_show(piece_id):
+    art = pieces.find_one({'_id': ObjectId(piece_id)})
+    return render_template('art_show.html', art=art)
 
 if __name__ == '__main__':
   app.run(debug=True)
