e69995a33db73cf5d42496333ba2b1f98f67a571
adgray09 <alexandergray37@yahoo.com>
2019-12-02 19:55:56 -0800

    heroku fix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from bson.objectid import ObjectId
 import os
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Contractor')
-client = MongoClient(host=host)
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 pieces = db.pieces
 
@@ -66,4 +66,4 @@ def pieces_delete(piece_id):
     return redirect(url_for('art_index'))
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
